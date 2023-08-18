e68bd7c13de74c41f0af7e578261c5b8d29d516f
vishal yadav <vishalyadav211296@gmail.com>
2023-08-18 11:14:37 +0530

    Math object comma operator date numbers clock unary turnary in java script

---
+++ Javascript/unaryoperator.js
@@ -0,0 +1,21 @@
+let x = 4;
+
+
+let y = -x;
+console.log(y);
+
+
+let a = '2';
+console.log(+a+3);
+
+//delete
+let obj = {name:"Vishal",mobile:2385265};
+console.log(obj);
+delete obj.mobile;
+console.log(obj);//{ name: 'Akarsh' }
+
+
+//typeof
+console.log(typeof(obj));//object
+console.log(typeof(a));//string
+console.log(typeof(x));//number
