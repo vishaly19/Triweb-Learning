fee97bc7d7727aa758786fc2d1aeb78cb3d93e81
vishal yadav <vishalyadav211296@gmail.com>
2023-08-20 15:07:36 +0530

    bitwise in js

---
+++ Javascript/bitwise.js
@@ -0,0 +1,21 @@
+let x1 = 3&2;  //and
+console.log(x1);  //2
+
+let x2 = 3|2;  //or
+console.log(x2);   //3
+
+let x3 = 3^2;  //zor
+console.log(x3);   //1
+
+let x4 = ~3;  //not
+console.log(x4);   //-4
+
+let x5 = 3<<2;  //Zero fill left shift
+console.log(x5);   //12
+
+let x6 = 3>>1;  //Signed right shift
+console.log(x6);  //1
+
+
+let x7 = 3>>>1;  //Zero fill right shift
+console.log(x7);  //1
