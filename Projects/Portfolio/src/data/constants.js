ed2e08fb8d771115452880f80a4a498dcd34c6fd
vishal yadav <vishalyadav211296@gmail.com>
2023-09-26 16:50:31 +0530

    Created Portfolio Using React

---
+++ Projects/Portfolio/src/data/constants.js
@@ -0,0 +1,242 @@
+export const Bio = {
+  name: "Vishal Yadav",
+  roles: [
+    "Full Stack Developer",
+    
+    "Mern Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/vishaly19",
+  resume:
+    "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/Vishal%20Y.pdf?alt=media&token=2be015f1-aa66-4061-8d5c-30670074838f",
+  linkedin: "https://www.linkedin.com/in/vishal-yadav-448290226/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3Bi6MYRLSfRzCqO5eRVrlbuQ%3D%3D",
+  twitter: "",
+  insta: "",
+  facebook: "https://www.facebook.com/?ref=tn_tnmn",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/expess.png?alt=media&token=cb5ddeb8-eed2-4042-8f16-9ec5a7173461",
+      },
+     
+      {
+        name: "Python",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/python-original.svg?alt=media&token=f12b4ad0-d7d2-42ea-b1d8-ea8423b3a83b",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/mysql-original-wordmark.svg?alt=media&token=d2384d7b-76bb-45b3-8b1c-ace78a08c902",
+      },
+     
+      {
+        name: "MongoDB",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/mongodb-original-wordmark.svg?alt=media&token=e27f06d6-bdc5-4823-91bd-585d0af0ebd5",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+     
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://github.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+ 
+  {
+    id: 3,
+    img: "",
+    role: "React Developer",
+    company: "RTech Solutions",
+    date: "Jul2021 - Jan 2023",
+    desc: "As an React developer at the RTech Solutions, I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with RTech Solutions, and workshops.",
+    skills: [
+      "Leadership",
+      "React.js",
+      "Html",
+      "Css",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/triweb.jpg?alt=media&token=c28062d0-0415-4445-b7c2-bbccca8e8c24",
+    role: "Mern Stack Developer Intern",
+    company: "TriwebAPI",
+    date: "Aug 2023 - Present",
+    desc: "As an Mern Stack Developer Intern at TriwebAPI from Aug 2023 to Present, I gained valuable hands-on experience in Mern STack development. This internship helped me enhance my skills in Mern Stack development and gave me an opportunity to work in a collaborative team environment.",
+    skills: [
+      "Html",
+      "Css",
+      "JavaScript",
+      "Mysql",
+      "Node Js",
+      "MongoDB",
+      "Express js",
+      "React js",
+      "TypeScript"
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Makhanlal Chaturvedi National University of Journalism and Communication, Bhopal",
+    date: "Jul 2016 - Jul 2019",
+    grade: "60%",
+    desc: "",
+    degree: "Bachelor's of Computer Applications (B.C.A)",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Ramdhari Diwedi Inter College, Deoria",
+    date: "",
+    grade: "72%",
+    desc: "I completed my class 12th education at Ramdhari Diwedi Inter College, Deoria, where I studied Science. ",
+    degree: "U.P Board(XII), Science",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "SMT. C.D.U.M.V Saroura Vishen Deoria",
+    date: "",
+    grade: "71%",
+    desc: "I completed my class 10th education at Shrimati Chandravati Uchchatar Madhyamik Vidyalay Saroura Vishen,Deoria, where I studied Science.",
+    degree: "U.P Board(X), Science",
+  },
+];
+
+export const projects = [
+  
+   
+ 
+  {
+    id: 6,
+    title: "Tic-Tac-Toe",
+    date: "Sep 2023",
+    description:
+      "Tic-tac-toe, noughts and crosses, or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O.",
+    image:
+     "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/istockphoto.jpg?alt=media&token=0ef2b316-0d29-41a4-a870-872c5c24a674",
+    tags: ["React Js", "Node JS"],
+    category: "web app",
+    github: "https://github.com/vishaly19/Triweb-Learning/commit/361ce134ce8d49e7eeaeedb113683912827683c6",
+    webapp: "",
+  },
+  {
+    id: 7,
+    title: "Quiz Manager App",
+    date: "Sep",
+    description:
+      "Quiz manager app using node mongo and javascript and postman",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/quixx.png?alt=media&token=bf9eaab0-2749-4b8c-8d85-2cf413f4cc12",
+    tags: ["Node js, Api, Javascript, Html, Yarn"],
+    category: "web app",
+    github: "https://github.com/vishaly19/Triweb-Learning/commit/e8593ce41620bf7ace4b21f1472de0229f74fd73",
+    webapp: "",
+  },
+  {
+    id: 8,
+    title: "On/Off bulb",
+    date: "Sep 2023",
+    description:
+      "In this Project we will write a code that changes the state of bulb from ON to OFF and by clicking on the image of bulb",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/imagesforproject-e74fa.appspot.com/o/On-Off-Bulb-01.png?alt=media&token=7a39d59b-5058-4b86-ab77-c292af9d9766",
+    tags: ["JavaScript"],
+    category: "web app",
+    github: "https://github.com/vishaly19/Triweb-Learning/commit/2aef8b081aa0816a1ead9bd7e189d8ed82b0d4a9",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  
+];
